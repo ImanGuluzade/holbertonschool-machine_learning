79afdf171108becea208cb17a4905f8deee4e914
ImanGuluzade <iman.guluzade@gmail.com>
2026-01-10 18:02:44 +0000

    Add 1-scatter exercise with magenta scatter plot

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatter():
+    """Plot a scatter plot of men's height vs weight."""
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+    
+    # Scatter plot in magenta
+    plt.scatter(x, y, color='magenta')
+    
+    # Labels and title
+    plt.xlabel("Height (in)")
+    plt.ylabel("Weight (lbs)")
+    plt.title("Men's Height vs Weight")
+    
+    # Show the plot
+    plt.show()
