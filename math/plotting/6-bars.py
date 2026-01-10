5cd13d9141c2ad922f911e7dba9fe7bf07bba909
ImanGuluzade <iman.guluzade@gmail.com>
2026-01-10 18:32:35 +0000

    Add math/plotting exercises 0-6 with main files

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -7,26 +7,22 @@ def bars():
     fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
 
-    # Names of people
     people = ['Farrah', 'Fred', 'Felicia']
-    
-    # Colors for fruits
-    colors = ['red', 'yellow', '#ff8000', '#ffe5b4']  # apples, bananas, oranges, peaches
-    
-    # Plot each fruit stacked
-    bottom = np.zeros(3)  # starting point for stacking
-    for i in range(4):
-        plt.bar(people, fruit[i], bottom=bottom, color=colors[i], width=0.5, label=['Apples', 'Bananas', 'Oranges', 'Peaches'][i])
-        bottom += fruit[i]  # increment bottom for next stack
-    
-    # Labels, title, y-axis
+    colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
+    labels = ['apples', 'bananas', 'oranges', 'peaches']
+    width = 0.5
+
+    # cumulative bottom for stacking
+    bottom = np.zeros(fruit.shape[1])
+
+    for i in range(fruit.shape[0]):
+        plt.bar(people, fruit[i], width=width, bottom=bottom,
+                color=colors[i], label=labels[i])
+        bottom += fruit[i]
+
     plt.ylabel('Quantity of Fruit')
-    plt.title('Number of Fruit per Person')
     plt.ylim(0, 80)
     plt.yticks(np.arange(0, 81, 10))
-    
-    # Add legend
+    plt.title('Number of Fruit per Person')
     plt.legend()
-    
-    # Show plot
     plt.show()
