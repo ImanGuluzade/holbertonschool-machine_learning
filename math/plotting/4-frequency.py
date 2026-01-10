4f2391010d2a878909b435c4dd79d823dda169c4
ImanGuluzade <iman.guluzade@gmail.com>
2026-01-10 18:38:46 +0000

    Fix 4-frequency.py: correct histogram, pass grader and pycodestyle

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -2,18 +2,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def frequency():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
-
-    # Histogram with bins every 10 units, bars outlined in black
-    bins = np.arange(0, 101, 10)
-    plt.hist(student_grades, bins=bins, edgecolor='black')
-
-    # Labels and title
-    plt.xlabel("Grades")
-    plt.ylabel("Number of Students")
-    plt.title("Project A")
-
-    # Print required output for auto-grader
-    print("The plot matches the reference.")
+    plt.figure(figsize=(6.4, 4.8))
+    plt.hist(student_grades, bins=np.arange(0, 101, 10),
+             edgecolor='black')
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+    plt.show()
