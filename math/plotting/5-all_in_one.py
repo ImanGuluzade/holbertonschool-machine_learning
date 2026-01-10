cc197376067ff2621801f3de09037c95d5bbd4e3
ImanGuluzade <iman.guluzade@gmail.com>
2026-01-10 18:27:44 +0000

    Add all-in-one plotting exercise with 5 subplots

---
+++ math/plotting/5-all_in_one.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+def all_in_one():
+    # Data for all plots
+    y0 = np.arange(0, 11) ** 3
+
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x1, y1 = np.random.multivariate_normal(mean, cov, 2000).T
+    y1 += 180
+
+    x2 = np.arange(0, 28651, 5730)
+    r2 = np.log(0.5)
+    t2 = 5730
+    y2 = np.exp((r2 / t2) * x2)
+
+    x3 = np.arange(0, 21000, 1000)
+    r3 = np.log(0.5)
+    t31 = 5730
+    t32 = 1600
+    y31 = np.exp((r3 / t31) * x3)
+    y32 = np.exp((r3 / t32) * x3)
+
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+
+    # Create figure
+    fig = plt.figure(figsize=(10, 8))
+    fig.suptitle("All in One", fontsize='x-small')
+
+    # 1) Line plot
+    ax1 = fig.add_subplot(3, 2, 1)
+    ax1.plot(np.arange(0, 11), y0, 'r-')
+    ax1.set_xlabel("x", fontsize='x-small')
+    ax1.set_ylabel("y", fontsize='x-small')
+    ax1.set_title("y = x^3", fontsize='x-small')
+
+    # 2) Scatter plot
+    ax2 = fig.add_subplot(3, 2, 2)
+    ax2.scatter(x1, y1, c='m', s=10)
+    ax2.set_xlabel("Height (in)", fontsize='x-small')
+    ax2.set_ylabel("Weight (lbs)", fontsize='x-small')
+    ax2.set_title("Men's Height vs Weight", fontsize='x-small')
+
+    # 3) Exponential decay plot
+    ax3 = fig.add_subplot(3, 2, 3)
+    ax3.plot(x2, y2)
+    ax3.set_xlabel("Time (years)", fontsize='x-small')
+    ax3.set_ylabel("Fraction Remaining", fontsize='x-small')
+    ax3.set_title("Exponential Decay of C-14", fontsize='x-small')
+    ax3.set_yscale('log')
+
+    # 4) Two exponential decay lines
+    ax4 = fig.add_subplot(3, 2, 4)
+    ax4.plot(x3, y31, 'r--', label='C-14')
+    ax4.plot(x3, y32, 'g-', label='Ra-226')
+    ax4.set_xlabel("Time (years)", fontsize='x-small')
+    ax4.set_ylabel("Fraction Remaining", fontsize='x-small')
+    ax4.set_title("Exponential Decay of Radioactive Elements", fontsize='x-small')
+    ax4.legend(fontsize='x-small')
+
+    # 5) Histogram (spanning last row: two columns)
+    ax5 = fig.add_subplot(3, 1, 3)
+    bins = np.arange(0, 101, 10)
+    ax5.hist(student_grades, bins=bins, edgecolor='black')
+    ax5.set_xlabel("Grades", fontsize='x-small')
+    ax5.set_ylabel("Number of Students", fontsize='x-small')
+    ax5.set_title("Project A", fontsize='x-small')
+
+    plt.tight_layout(rect=[0, 0, 1, 0.95])
+    plt.show()
