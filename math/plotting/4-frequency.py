40d74b9f0c6c11fef5d5e224e37b806b5dbd1d47
ImanGuluzade <iman.guluzade@gmail.com>
2026-01-10 19:10:42 +0000

    Fix 4-frequency: exact bins and outlined bars for autograder

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+"""Module to plot a histogram of grades"""
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 def frequency():
     """
@@ -10,23 +12,23 @@ def frequency():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-    
-    # 1. Define bins from 0 to 100 in steps of 10
+
+    # Define bins from 0 to 100 in steps of 10
     bins = np.arange(0, 101, 10)
-    
-    # 2. Plot the histogram with black edges
+
+    # Plot the histogram with black edges
     plt.hist(student_grades, bins=bins, edgecolor='black')
-    
-    # 3. Set labels and title
+
+    # Set labels and title
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title("Project A")
-    
-    # 4. Set the exact axis limits to match the reference image
+
+    # Set the exact axis limits to match the reference image
     plt.xlim(0, 100)
     plt.ylim(0, 30)
-    
-    # 5. Set the x-ticks to be every 10 units
+
+    # Set the x-ticks to be every 10 units
     plt.xticks(bins)
-    
+
     plt.show()
