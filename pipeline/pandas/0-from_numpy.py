5523fb281cd3ef70bd152c1b0a0c91cd74275966
ImanGuluzade <iman.guluzade@gmail.com>
2026-01-10 16:01:54 +0000

    Add pandas from_numpy task

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import string
+
+
+def from_numpy(array):
+    cols = list(string.ascii_uppercase[:array.shape[1]])
+    return pd.DataFrame(array, columns=cols)
