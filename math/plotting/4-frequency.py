c9a7a5506416632a87454757c68aca873efd13c0
ImanGuluzade <iman.guluzade@gmail.com>
2026-01-10 18:52:37 +0000

    Fix 4-frequency.py: correct histogram plot for Holberton checker

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Plot a histogram of student grades for Project A
+Plot a histogram of student scores for Project A.
 """
 
 import numpy as np
@@ -8,12 +8,21 @@ import matplotlib.pyplot as plt
 
 
 def frequency():
-    """Plots a histogram of student grades."""
+    """
+    Creates a histogram of 50 student grades:
+    - X-axis: Grades, bins every 10 units
+    - Y-axis: Number of Students
+    - Bars outlined in black
+    - Title: Project A
+    """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
+
     plt.figure(figsize=(6.4, 4.8))
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
-    plt.xlabel('Grades')
-    plt.ylabel('Number of Students')
-    plt.title('Project A')
-    # do not call plt.show() here
+    bins = np.arange(0, 110, 10)  # Ensure bins go from 0 to 100 inclusive
+    plt.hist(student_grades, bins=bins, edgecolor='black')
+
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
+    plt.show()
