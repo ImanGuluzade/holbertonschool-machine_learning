f3ef4a592bf5befb36829c38f21d68f7ef53d9db
ImanGuluzade <iman.guluzade@gmail.com>
2026-02-04 19:08:55 +0000

    Add 13-cats_got_your_tongue.py - concatenate numpy arrays along axis

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -6,6 +6,11 @@ along a specific axis.
 The np_cat function returns a new numpy.ndarray with the concatenated 
 result along the given axis.
 """
+
+import numpy as np
+ø
+
+
 def  np_cat(mat1,mat2,axis=0):
      """Concatenates two numpy.ndarrays along a specified axis"""
      return np.concatenate((mat1,mat2),axis=axis)
