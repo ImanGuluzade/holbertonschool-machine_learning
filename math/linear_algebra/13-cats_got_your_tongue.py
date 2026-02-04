3487d81957284ae03675f6220ba83709d63066cf
ImanGuluzade <iman.guluzade@gmail.com>
2026-02-04 19:26:15 +0000

    Fix PEP8 for 13-cats_got_your_tongue.py

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """
-This module containts a function to concatenate two numpy.ndarrays
+This module contains a function to concatenate two numpy.ndarrays
 along a specific axis.
 
-The np_cat function returns a new numpy.ndarray with the concatenated 
+The np_cat function returns a new numpy.ndarray with the concatenated
 result along the given axis.
 """
 
 import numpy as np
 
 
-def  np_cat(mat1, mat2, axis=0):
-     """Concatenates two numpy.ndarrays along a specified axis"""
-     return np.concatenate((mat1, mat2), axis=axis)
+def np_cat(mat1, mat2, axis=0):
+    """Concatenates two numpy.ndarrays along a specified axis"""
+    return np.concatenate((mat1, mat2), axis=axis)
