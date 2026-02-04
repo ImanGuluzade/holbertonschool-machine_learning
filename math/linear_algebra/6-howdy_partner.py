a054b8a643ac671f7e26d1ddb514f2d61af50795
ImanGuluzade <iman.guluzade@gmail.com>
2026-02-04 18:22:27 +0000

    Add 6-howdy_partner.py - cat_arrays function

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module contains a function to concatenate two arrays.
+
+The cat_arrays function returns a new list containing all elements
+from arr1 followed by all elements from arr2.
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """Concatenates two arrays and returns a new list"""
+    return arr1 + arr2
