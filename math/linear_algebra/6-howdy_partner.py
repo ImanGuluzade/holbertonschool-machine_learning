e04f61c1e17c6210272cd42420d3f16e72fbf5e5
ImanGuluzade <iman.guluzade@gmail.com>
2026-02-04 19:50:49 +0000

    Fix cat_arrays to pass checker

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Concatenate two arrays."""
 
+
 def cat_arrays(arr1, arr2):
     """Returns a new list containing all elements of arr1 followed by arr2"""
     return arr1 + arr2
