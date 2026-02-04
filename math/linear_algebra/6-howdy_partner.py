95f8dd6320b3f89660c5bc33ce8d8ed866a460da
ImanGuluzade <iman.guluzade@gmail.com>
2026-02-04 19:41:31 +0000

    Fix 6-howdy_partner.py - correct array concatenation

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -9,9 +9,4 @@ from arr1 followed by all elements from arr2.
 
 def cat_arrays(arr1, arr2):
     """Concatenates two lists and returns a new list"""
-    new_list = []
-    for el in arr1:
-        new_list.append(el)
-    for el in arr2:
-        new_list.append(el)
-    return new_list
+    return list(arr1) + list(arr2)
