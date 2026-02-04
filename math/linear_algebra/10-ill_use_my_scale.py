a0b0ce9c266774d549b796281c37826287a59542
ImanGuluzade <iman.guluzade@gmail.com>
2026-02-04 18:46:17 +0000

    Add 10-ill_use_my_scale.py - np_shape function

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module contains a function to get the shape of a numpy.ndarray.
+
+The np_shape function returns a tuple of integers representing the
+dimensions of the input numpy array.
+"""
+
+
+def np_shape(matrix):
+    """Returns the shape of a numpy.ndarray"""
+    return matrix.shape
