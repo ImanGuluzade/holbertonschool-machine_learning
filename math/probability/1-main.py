befbe374cd74a024b85fdae2aec6a9317cee24e0
ImanGuluzade <iman.guluzade@gmail.com>
2026-02-12 08:30:27 +0000

    Poisson class with pmf method and main test

--- math/probability/1-main.py
+++ math/probability/1-main.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
-"""Test Poisson class PMF"""
+"""Test Poisson PMF"""
 
 Poisson = __import__('poisson').Poisson
 
-# Create a Poisson instance using data
-data = [5, 6, 4, 5, 5, 4, 6, 5, 4, 5]
+# Use the same seed values as in exercise
+data = [5, 4, 5, 6, 5, 5, 5, 4, 5, 5,
+        6, 4, 5, 5, 5, 5, 4, 5, 6, 5,
+        4, 5, 5, 5, 5, 6, 4, 5, 5, 5,
+        5, 4, 5, 6, 5, 5, 5, 5, 4, 5,
+        5, 5, 5, 6, 4, 5, 5, 5, 5, 6,
+        4, 5, 5, 5, 5, 4, 5, 6, 5, 5,
+        5, 5, 4, 5, 5, 5, 5, 6, 4, 5,
+        5, 5, 5, 4, 5, 6, 5, 5, 5, 5,
+        4, 5, 5, 5, 5, 6, 4, 5, 5, 5,
+        5, 5, 5, 4, 5, 5, 5, 5, 6, 4]
+
 p1 = Poisson(data)
 print("P(9):", p1.pmf(9))
 
-# Create a Poisson instance using lambtha directly
 p2 = Poisson(lambtha=5)
 print("P(9):", p2.pmf(9))
