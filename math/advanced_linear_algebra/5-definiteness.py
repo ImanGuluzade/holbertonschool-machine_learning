df968fc68c4656e2f78f795d2eda7df1c5c75e25
ImanGuluzade <iman.guluzade@gmail.com>
2026-02-12 07:29:47 +0000

    Add definiteness function using NumPy, with main

---
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Calculates the definiteness of a matrix using NumPy"""
+
+import numpy as np
+
+
+def definiteness(matrix):
+    """Determines if a matrix is positive/negative definite or semi-definite"""
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("matrix must be a numpy.ndarray")
+
+    if len(matrix.shape) != 2 or matrix.shape[0] != matrix.shape[1]:
+        return None
+
+    try:
+        eigvals = np.linalg.eigvals(matrix)
+    except np.linalg.LinAlgError:
+        return None
+
+    pos = np.all(eigvals > 0)
+    pos_semi = np.all(eigvals >= 0) and np.any(eigvals == 0)
+    neg = np.all(eigvals < 0)
+    neg_semi = np.all(eigvals <= 0) and np.any(eigvals == 0)
+
+    if pos:
+        return "Positive definite"
+    if pos_semi:
+        return "Positive semi-definite"
+    if neg:
+        return "Negative definite"
+    if neg_semi:
+        return "Negative semi-definite"
+    if np.any(eigvals > 0) and np.any(eigvals < 0):
+        return "Indefinite"
+
+    return None
