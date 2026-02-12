b207f504c46d8b89b7f3dc3447b8d3ce9bc48709
ImanGuluzade <iman.guluzade@gmail.com>
2026-02-12 07:32:51 +0000

    Fix definiteness: check symmetry before eigenvalues

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
-"""Calculates the definiteness of a matrix using NumPy"""
+"""Calculates the definiteness of a symmetric matrix using NumPy"""
 
 import numpy as np
 
 
 def definiteness(matrix):
-    """Determines if a matrix is positive/negative definite or semi-definite"""
+    """Determines if a symmetric matrix is positive/negative definite or semi-definite"""
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
 
     if len(matrix.shape) != 2 or matrix.shape[0] != matrix.shape[1]:
+        return None
+
+    # Check symmetry
+    if not np.allclose(matrix, matrix.T):
         return None
 
     try:
