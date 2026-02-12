a8a15a2cb202d7bfc37e299e5b29febeecff4c3b
ImanGuluzade <iman.guluzade@gmail.com>
2026-02-12 06:45:13 +0000

    Add determinant function and main test for advanced_linear_algebra

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """Calculates the determinant of a matrix"""
 
+
 def determinant(matrix):
     """Calculates the determinant of a square matrix"""
     # Check if matrix is a list of lists
-    if not isinstance(matrix, list) or any(not isinstance(row, list) for row in matrix):
+    if (not isinstance(matrix, list)
+            or any(not isinstance(row, list) for row in matrix)):
         raise TypeError("matrix must be a list of lists")
 
     # 0x0 matrix
@@ -21,12 +23,13 @@ def determinant(matrix):
     if n == 1:
         return matrix[0][0]
     if n == 2:
-        return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
 
     # Recursive calculation for n > 2
     det = 0
     for c in range(n):
-        minor = [[matrix[i][j] for j in range(n) if j != c] for i in range(1, n)]
+        minor = [[matrix[i][j] for j in range(n) if j != c]
+                 for i in range(1, n)]
         det += ((-1) ** c) * matrix[0][c] * determinant(minor)
 
     return det
