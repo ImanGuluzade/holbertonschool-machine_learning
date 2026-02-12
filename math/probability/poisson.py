fa92cf786b758df197704c6d9eaf094b86db9108
ImanGuluzade <iman.guluzade@gmail.com>
2026-02-12 09:51:23 +0000

    Add pmf and cdf methods to Poisson class

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 """
-Poisson distribution class
+Poisson distribution module
 """
 
 
 class Poisson:
     """
-    Represents a Poisson distribution
+    Class that represents a Poisson distribution
     """
 
     def __init__(self, data=None, lambtha=1.0):
         """
-        Initialize Poisson distribution
+        Initialization of the class
         """
         if data is None:
             if lambtha <= 0:
@@ -28,22 +28,33 @@ class Poisson:
         """
         Calculates the value of the PMF for a given number of successes
         """
-        # Convert k to integer
         k = int(k)
-
-        # If k is out of range
         if k < 0:
             return 0
 
         # Euler's number constant
         e = 2.7182818285
 
-        # Calculate k factorial (k!)
-        factorial = 1
+        # Calculate factorial of k
+        fact = 1
         for i in range(1, k + 1):
-            factorial *= i
+            fact *= i
 
-        # Calculate PMF: (lambtha^k * e^-lambtha) / k!
-        pdf_val = (self.lambtha ** k * (e ** -self.lambtha)) / factorial
+        # PMF Formula: (e^-L * L^k) / k!
+        probability = ((e ** -self.lambtha) * (self.lambtha ** k)) / fact
+        return probability
 
-        return pdf_val
+    def cdf(self, k):
+        """
+        Calculates the value of the CDF for a given number of successes
+        """
+        k = int(k)
+        if k < 0:
+            return 0
+
+        # Cumulative probability is the sum of PMFs from 0 to k
+        cumulative = 0
+        for i in range(k + 1):
+            cumulative += self.pmf(i)
+
+        return cumulative
