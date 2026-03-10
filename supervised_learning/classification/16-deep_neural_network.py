8ba31b02a09a777020da868e6c00ffe0c5262d22
ImanGuluzade <iman.guluzade@gmail.com>
2026-03-10 12:18:53 +0000

    Ultra-clean DNN init for checker compliance

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -19,24 +19,17 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
-
         for i in range(len(layers)):
             if not isinstance(layers[i], int) or layers[i] <= 0:
                 raise TypeError("layers must be a list of positive integers")
-
             if i == 0:
-                prev_size = nx
+                prev = nx
             else:
-                prev_size = layers[i - 1]
-
+                prev = layers[i - 1]
             # He et al. initialization
-            # Using parentheses to break the line for E501 compliance
-            self.weights["W" + str(i + 1)] = (
-                np.random.randn(layers[i], prev_size) *
-                np.sqrt(2 / prev_size)
-            )
+            he = np.sqrt(2 / prev)
+            self.weights["W" + str(i + 1)] = np.random.randn(layers[i], prev) * he
             self.weights["b" + str(i + 1)] = np.zeros((layers[i], 1))
