7f33ca1e8794abcf169d803949f02ad435307e90
ImanGuluzade <iman.guluzade@gmail.com>
2026-03-15 10:03:03 +0000

    Add Adam TensorFlow implementation and manual Learning Rate Decay

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -13,7 +13,7 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
         alpha: the learning rate
         beta1: the weight used for the first moment
         beta2: the weight used for the second moment
-        epsilon: small number to avoid division by zero
+        epsilon: a small number to avoid division by zero
         var: numpy.ndarray containing the variable to be updated
         grad: numpy.ndarray containing the gradient of var
         v: the previous first moment of var
@@ -21,22 +21,24 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
         t: the time step used for bias correction
 
     Returns:
-        The updated variable, the new first moment, 
+        The updated variable, the new first moment,
         and the new second moment, respectively
     """
     # 1. Update first moment (Momentum)
-    new_v = (beta1 * v) + ((1 - beta1) * grad)
+    v_new = (beta1 * v) + ((1 - beta1) * grad)
 
     # 2. Update second moment (RMSProp)
-    new_s = (beta2 * s) + ((1 - beta2) * (grad ** 2))
+    s_new = (beta2 * s) + ((1 - beta2) * (grad ** 2))
 
     # 3. Bias correction for first moment
-    v_corrected = new_v / (1 - (beta1 ** t))
+    v_corrected = v_new / (1 - (beta1 ** t))
 
     # 4. Bias correction for second moment
-    s_corrected = new_s / (1 - (beta2 ** t))
+    s_corrected = s_new / (1 - (beta2 ** t))
 
     # 5. Update the variable
-    new_var = var - (alpha * (v_corrected / (np.sqrt(s_corrected) + epsilon)))
+    # We break this into parts to keep line length under 79 chars
+    denom = np.sqrt(s_corrected) + epsilon
+    var_new = var - (alpha * (v_corrected / denom))
 
-    return new_var, new_v, new_s
+    return var_new, v_new, s_new
