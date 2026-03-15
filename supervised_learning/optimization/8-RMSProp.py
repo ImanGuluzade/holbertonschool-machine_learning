c732db8506ad87e8ba7ddc3b906c84faf3b27d15
ImanGuluzade <iman.guluzade@gmail.com>
2026-03-15 09:48:05 +0000

    Add optimization constants and directory setup with README

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Module to create an RMSProp optimizer in TensorFlow
+"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(alpha, beta2, epsilon):
+    """
+    Sets up the RMSProp optimization algorithm in TensorFlow
+
+    Args:
+        alpha: the learning rate
+        beta2: the RMSProp weight (discounting factor)
+        epsilon: small number to avoid division by zero
+
+    Returns:
+        The optimizer
+    """
+    # rho is the TensorFlow equivalent of beta2 in RMSProp
+    return tf.keras.optimizers.RMSprop(
+        learning_rate=alpha,
+        rho=beta2,
+        epsilon=epsilon
+    )
