023e17a02bf753d62e25600f56ba81c2c0c1f309
ImanGuluzade <iman.guluzade@gmail.com>
2026-05-31 17:51:05 +0000

    Add change_brightness function with perfect pycodestyle layout

---
+++ pipeline/data_augmentation/4-brightness.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module containing the change_brightness function for data augmentation.
+"""
+import tensorflow as tf
+
+
+def change_brightness(image, max_delta):
+    """
+    Randomly changes the brightness of an image.
+
+    Parameters:
+    image: a 3D tf.Tensor containing the image to change
+    max_delta: maximum amount the image should be brightened or darkened
+
+    Returns:
+    The brightness-adjusted image tensor
+    """
+    return tf.image.random_brightness(image, max_delta=max_delta)
