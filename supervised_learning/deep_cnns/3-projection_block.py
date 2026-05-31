e0e243ee029667f24baa018409877f2839bea4e1
ImanGuluzade <iman.guluzade@gmail.com>
2026-05-31 17:23:43 +0000

    Fix pycodestyle line length restriction on documentation

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -17,7 +17,8 @@ def projection_block(A_prev, filters, s=2):
              F3 is the number of filters in the 3x3 convolution
              F12 is the number of filters in the second 1x1 convolution
                  as well as the 1x1 convolution in the shortcut connection
-    s: stride of the first convolution in both main path and shortcut connection
+    s: stride of the first convolution in both main path and shortcut
+       connection
 
     Returns:
     The activated output of the projection block
